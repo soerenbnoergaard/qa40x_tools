7df3371c62aeefd31c3b36e3c3adb2237b7437a6
Soeren Boegeskov Noergaard <soerenbnoergaard@gmail.com>
2026-05-25 14:14:04 +0200

    Working on measuring impulse reponse

--- measure_impulse_response.py
+++ measure_impulse_response.py
@@ -3,52 +3,135 @@ import matplotlib.pyplot as plt
 import scipy.signal
 import qa40x as qa
 
-def measure(label=""):
-    inst = qa.Qa40x()
-    inst.acquire()
-    fodata = inst.measure_played_spectrum()
-    todata = inst.measure_played_waveform()
-    fidata = inst.measure_recorded_spectrum()
-    tidata = inst.measure_recorded_waveform()
+def expochirp(f1, f2, N, fs):
+    T = N / fs
+    t = np.arange(N) / fs
+    w1 = 2 * np.pi * f1
+    w2 = 2 * np.pi * f2
+    return np.sin(
+        (w1 * T) / (np.log(w2 / w1)) *
+        (np.exp(t / T * np.log(w2 / w1)) - 1)
+    )
 
+def measure(label="", amplitude_dBV=-20, num_points=256 * 1024):
+    inst = qa.Qa40x()
+    assert amplitude_dBV < 10
+    fs = qa.SAMPLE_RATE_Hz
+
+    # Using a ESS waveform instead of the built-in ExpoChirp, to be in better control.
+    # Still using a 10-octave sweep up to fs/2
+    #
+    # farina2007advancements_in_impulse_response_measurements_by_sine_sweeps
+
+    f2 = fs/2
+    f1 = f2 / (2**10)
+
+    # Leaving some silence in the beginning and the end
+    x = np.zeros(num_points)
+    n_start = 1 * num_points // 8
+    n_stop = 7 * num_points // 8
+    N = n_stop - n_start
+    A = 10**(amplitude_dBV / 20)
+    # R = ramp(10000, 0, N)
+    x[n_start:n_stop] = A * expochirp(f1, f2, N, fs)
+    t = np.arange(num_points) / fs
+
+    inst.set_buffer_size(N)
+    inst.acquire_custom_waveform(x)
+    meas = inst.measure_recorded_waveform()
     data = {
-        "fodata": fodata,
-        "tidata": tidata,
-        "todata": todata,
-        "fidata": fidata,
+        "frequency_start_Hz": f1,
+        "frequency_stop_Hz": f2,
+        "time_s": t.tolist(),
+        "input_V": x.tolist(),
+        "output_left_V": meas["Left"],
+        "output_right_V": meas["Right"],
+        "index_start": n_start,
+        "index_stop": n_stop,
     }
     qa.dump_data(qa.generate_filename(label, "impulse_response"), data)
     return data
 
+def ramp(ramp_up, ramp_down, num_samples):
+    r = np.ones(num_samples)
+
+    for n in range(ramp_up):
+        r[n] = n * 1 / ramp_up
+
+    for n in range(ramp_down):
+        r[num_samples - n - 1] = n * 1 / ramp_down
+
+    return r
+
 def analyze(data):
-    t = data["tidata"]["Time"]
-    x = data["todata"]["Left"]
-    y = data["tidata"]["Left"]
+    t = data["time_s"]
+    x = data["input_V"]
+    y = data["output_left_V"]
+    n_start = data["index_start"]
+    n_stop = data["index_stop"]
+    f_start = data["frequency_start_Hz"]
+    f_stop = data["frequency_stop_Hz"]
+    N = len(x)
 
-    f = data["fidata"]["Frequency"]
-    X = data["fodata"]["Left"]
-    Y = data["fidata"]["Left"]
-
-    fig, ax = plt.subplots()
+    fig, axs = plt.subplots(4, 1, figsize=(8, 8))
+    ax = axs[0]
     ax.plot(t, x)
     ax.plot(t, y)
 
-    # TODO: Deconvolution + compare with the extracted output spectrum from Qa40x
-    # https://hal.science/hal-02504321v1/document
-    # https://www.researchgate.net/publication/237724101_DIRECTOR%27S_CUT_INCLUDING_PREVIOUSLY_UNRELEASED_MATERIAL_AND_SOME_CORRECTIONS
-    # https://pyfar-gallery.readthedocs.io/en/latest/gallery/no_binder/impulse_response_measurement.html
-    # https://www.carminecella.com/teaching/IR_calc.pdf
+    # Construct the "inverse filter" from the input and apply amplitude modulation to invert the pink noise slope
+    # Amplitude modulation: Starts at 0 dB, ends at -6 * log2(w2 / w1).
+    #
+    # - farina2000simultaneous_measurement_of_impulse_response_and_distortion_with_a_swept-sine_technique
+    # - holters2009impulse_response_measurement_techniques_and_their_applicability_in_the_real_world
 
-    fig, ax = plt.subplots()
-    ax.semilogx(f, 20 * np.ma.log10(X))
-    ax.semilogx(f, 20 * np.ma.log10(Y))
+    log2 = lambda _x: np.log(_x) / np.log(2)
+    L = n_stop - n_start
+    a_start_dB = 0
+    a_stop_dB = a_start_dB - 6 * log2(f_stop / f_start)
+    a_mod = 10**(np.linspace(a_start_dB, a_stop_dB, L) / 20)
+
+    x_inv_section = a_mod * np.flipud(x[n_start:n_stop])
+    x_inv = np.zeros(N)
+    x_inv[N - n_stop - 1:N - n_start - 1] = x_inv_section
+
+    ax = axs[1]
+    ax.plot(x)
+    ax.plot(x_inv)
+
+    # Do deconvolution to obtain impulse response
+    h_est = scipy.signal.convolve(y, x_inv, "full")
+    n_est_mid = len(h_est) // 2
+    n_est = (np.arange(len(h_est)) - n_est_mid)
+    n_est_low = n_est_mid - 1000
+    n_est_high = n_est_mid + 10000
+
+    ax = axs[2]
+    ax.plot(n_est, h_est)
+    ax.axvline(n_est_low - n_est_mid)
+    ax.axvline(n_est_high - n_est_mid)
+    ax.set_xlim(n_est_low - n_est_mid - 100, n_est_high - n_est_mid + 100)
+
+    # Extract
+    h_est = h_est[n_est_low : n_est_high]
+
+    # Calculate frequency response
+    H_est = np.abs(np.fft.fft(h_est))
+    H_est = H_est[0:len(H_est)//2]
+    f_est = np.linspace(0, 24000, len(H_est))
+
+    ax = axs[3]
+    ax.semilogx(f_est, 20*np.log10(H_est))
     ax.set_xlim(20, 20e3)
-    ax.set_ylim(-170, 20)
+    # ax.set_ylim(-170, 20)
+
+    fig.tight_layout()
+
+    # # TODO: Handle pre-ringing
 
 
 if __name__ == "__main__":
-    # data = measure()
-    # analyze(data)
+    data = measure()
+    analyze(data)
 
-    analyze(qa.load_data("_impulse_response_2026-05-24_123051.json.gz"))
+    # analyze(qa.load_data("_impulse_response_2026-05-25_140658.json.gz"))
     plt.show()
